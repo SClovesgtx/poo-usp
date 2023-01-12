462fd58b00cc7bc76622b6dc227b1644d3e4ccea
Cloves Paiva <cloves.paiva@extremedigital.com.br>
2023-01-12 08:22:15 -0300

    comentarios

--- heranca.py
+++ heranca.py
@@ -15,6 +15,9 @@ class Triangulo(Poligono):
         super().__init__(numero_de_lados=3)
     
     def area(self):
+        """
+        Aqui eu adiciono um método que especializa a classe filha.
+        """
         a, b, c = self.lados
         s = (a + b + c) / 2
         area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
@@ -24,8 +27,32 @@ class Triangulo(Poligono):
 class Retangulo(Poligono):
     def __init__(self) -> None:
         super().__init__(numero_de_lados=4)
+    
+    def le_dados(self):
+        """
+        Aqui eu altero o comportamento do método
+        da classe mãe
+        """
+        lado1 = float(input(f"Digite o tamanho da altura: \n"))
+        lado2 = float(input(f"Digite o tamanho do comprimento: \n"))
+        self.lados[0] = self.lados[2] = lado1
+        self.lados[1] = self.lados[3] = lado2
+
+    def area(self):
+        print(f"Aréa do retêngulo é {self.lados[0] * self.lados[1]}.\n")
+
+    def diagonal(self):
+        print(f"A diagonal do retângulo mede {(self.lados[0]**2 + self.lados[1]**2) ** 0.5}.\n")
 
 
 triangulo = Triangulo()
+ # como o triangulo não implementa este método ele sobe para suas 
+ # para as suas superclasses até encontrar a primeira super classe
+ # que implementa este método e o executa (que no nosso caso é a classe Poligono)
 triangulo.le_dados()
 triangulo.area()
+
+ret = Retangulo()
+ret.le_dados()
+ret.area()
+ret.diagonal()
